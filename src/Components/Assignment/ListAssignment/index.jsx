1e25e8a2f2bff0a880230f5afec1419c675c0914
Tran Phuoc Loc <giongnhuntlf@gmail.com>
2021-11-24 19:01:27 +0700

    Fix react smart feature

--- src/Components/Assignment/ListAssignment/index.jsx
+++ src/Components/Assignment/ListAssignment/index.jsx
@@ -11,6 +11,7 @@ import ClassroomTabs from '../../ClassroomTabs'
 import NavbarAddButton from '../../NavbarAddButton'
 import AddAssignmentFormDialog from '../AddAssignmentFormDialog'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { set } from 'date-fns'
 
 export default function ListAssignment() {
     const [error, setError] = useState(null)
@@ -32,35 +33,38 @@ export default function ListAssignment() {
         return arr; // for testing
     };
     async function handleOnDragEnd(result){
-        console.log(result);
+        // console.log(result);
         if (!result.destination) return;
         if(result.destination.index === result.source.index) return;
-        console.log('Before:\n',items);
+        // console.log('Before:\n',items);
         let items_list = Array.from(items); 
         items_list = array_move(items_list, result.source.index, result.destination.index);
-        console.log('After:\n', items_list);
+        // console.log('After:\n', items_list);
         setItems(items_list);
-        console.log('After-items:\n', items);
+        // console.log('After-items:\n', items);
 
-
+        
         //Sending changed data to APIs
         const response = await assignmentAPI.rearrangeAssignments(params.classroomId, items_list);
         if (!response.ok) 
             console.log('Can not update assignments positions');
         else
             console.log('Assignments positions updated');	
+        setToggleAddNew(!toggleAddNew)
     }
     useEffect(() => {
         
         async function fetchData() {
             const response = await assignmentAPI.getAllAssignments(params.classroomId)
-            
             if (!response.ok) {
                 setError(response.status)
                 setItems([])
             }
             else {
+                setItems([])
+                console.log('reload')
                 const response_data = await response.json()
+                console.log(response_data)
                 setItems(response_data)
                 setLoading(false)
             }
@@ -99,7 +103,7 @@ export default function ListAssignment() {
                                 <List className='container__assignment-list'ref={provided.innerRef} {...provided.droppableProps}>{
                                     items.map((item, index) => {
                                         const cloneItem = {...item}
-                                        // console.log(cloneItem, index)
+                                        // console.log(item)
                                         return (<Draggable key={index} draggableId={index.toString()} index={index}>
                                                     {(provided) => {
                                                         return (<div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
