51ee841762c55dfdee4cfd59b2f2b823e0edd631
Tran Phuoc Loc <giongnhuntlf@gmail.com>
2022-01-10 02:02:17 +0700

    Join class room by code

--- src/Components/Classroom/ClassroomsList/index.jsx
+++ src/Components/Classroom/ClassroomsList/index.jsx
@@ -1,11 +1,26 @@
-import { React, useState, useEffect, useContext } from 'react'
+import { React, useState, useEffect, useContext, forwardRef } from 'react'
 import ClassroomCard from '../ClassroomCard'
 import classroomAPI from '../../../APIs/classroomAPI';
 import '../index.scss'
 import { NewClassroomAddedContext } from '../../../Context/NewClassroomAddedContext';
 import { NavbarElContext } from '../../../Context/GlobalContext';
 import NavbarAddButton from '../../NavbarAddButton';
+import { Stack, Button, Typography } from '@mui/material'
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import FormModal from '../FormModal';
+import accountAPI from '../../../APIs/accountAPI';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+const Alert = forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
 
 export default function ClassroomsList () {
     const [error, setError] = useState(null);
@@ -14,6 +29,10 @@ export default function ClassroomsList () {
     const [add,] = useContext(NewClassroomAddedContext)
     const [,setNavbarEl] = useContext(NavbarElContext)
     const [openAddModal, setOpenAddModal] = useState(false)
+    const [openJoinModal, setOpenJoinModal] = useState(false)
+    const [inviteCode, setInviteCode] = useState("")
+    const [snackbarState, setSnackbarState] = useState({severity:"", content:"", open:false})
+
 
     useEffect(() => {
         const cookData = raw => {
@@ -37,6 +56,24 @@ export default function ClassroomsList () {
         
     }, [add, setNavbarEl])
 
+    const handleCloseSnackbar = () => {
+        setSnackbarState({...snackbarState, open: false})
+    }
+
+    const handleJoin = async () => {
+        setSnackbarState({
+            severity: 'warning',
+            content: "Loading ...",
+            open: true
+        })
+        const response = await accountAPI.joinClassroom(inviteCode)
+        setSnackbarState({
+            severity: response.ok ? 'success' : 'error',
+            content: response.ok ? "OK" : `Error ${response.status}: ${response.statusText}`,
+            open: true
+        })
+    }
+
 
     if (error) {
         return <div>Error: {error.message}</div>;
@@ -44,34 +81,72 @@ export default function ClassroomsList () {
         return <div>Loading...</div>;
     } else {
         return (
-            <div 
-                className='classrooms-list' 
-                style={{
-                    display: 'flex',
-                    flexWrap: 'wrap',
-                    width: '100%',
-                    marginLeft: '-10px',
-                    padding: '1rem 0'
-                }}
-            >
-                <FormModal 
-                    header="Add Class"
-                    openStatus={openAddModal}
-                    handleClose={() => setOpenAddModal(false)}
-                />
-                {items.map((item, index) => (
-                    <ClassroomCard
-                        id={item.id}
-                        key={item.id}
-                        header={item.section}
-                        title={item.name}
-                        subTitle={item.description}
-                        createdAt={item.createdAt}
-                        creator={item.creator}
-                        actionTitle='Detail'
-                        imgHref={`https://picsum.photos/id/${index}/100/200`}
+            <Stack sx={{ p: 1}} direction={"column"}>
+                <Snackbar open={snackbarState.open} autoHideDuration={3000} onClose={handleCloseSnackbar}>
+                    <Alert onClose={handleCloseSnackbar} severity={snackbarState.severity} sx={{ width: '100%' }}>
+                        {snackbarState.content}
+                    </Alert>
+                </Snackbar>
+                <Dialog open={openJoinModal} onClose={() => setOpenJoinModal(false)}>
+                    <DialogTitle>Join</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            To a classroom enter its invite code.
+                        </DialogContentText>
+                        <TextField
+                            value={inviteCode}
+                            autoFocus
+                            margin="dense"
+                            id="inviteCode"
+                            label="Invite Code"
+                            type="text"
+                            fullWidth
+                            variant="standard"
+                            onChange={e => setInviteCode(e.target.value)}
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={() => setOpenJoinModal(false)}>Cancel</Button>
+                        <Button onClick={handleJoin}>Join</Button>
+                    </DialogActions>
+                </Dialog>
+                <Stack direction={"row"} alignItems={"center"} justifyContent={"space-between"} spacing={2}>
+                    <Typography varian="button" fontSize={28}>
+                        <strong>Classrooms</strong>
+                    </Typography>
+                    <Button variant='contained' onClick={() => setOpenJoinModal(true)}>Join</Button>
+                </Stack>
+                <div 
+                    className='classrooms-list' 
+                    style={{
+                        display: 'flex',
+                        flexWrap: 'wrap',
+                        width: '100%',
+                        marginLeft: '-10px',
+                        padding: '1rem 0'
+                    }}
+                >
+
+                    <FormModal 
+                        header="Add Class"
+                        openStatus={openAddModal}
+                        handleClose={() => setOpenAddModal(false)}
                     />
-                ))}
-            </div>)
+                    {items.map((item, index) => (
+                        <ClassroomCard
+                            id={item.id}
+                            key={item.id}
+                            header={item.section}
+                            title={item.name}
+                            subTitle={item.description}
+                            createdAt={item.createdAt}
+                            creator={item.creator}
+                            actionTitle='Detail'
+                            imgHref={`https://picsum.photos/id/${index}/100/200`}
+                        />
+                    ))}
+                </div>
+            </Stack>
+            )
     }
 }
